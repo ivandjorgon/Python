6996c6b95959d5a52e826b7f10bfc696525b53dd
ivandjorgon <38181521+ivandjorgon@users.noreply.github.com>
2018-04-08 13:04:47 -0400

    Update login method

--- PythonTests/login_method.py
+++ PythonTests/login_method.py
@@ -7,10 +7,10 @@ class login_method():
         self.driver = driver
 
     # Method that allows us to find the value of any cell on the test 6 div grid
-    def login_method_purefacts(self, row, column):
+    def login_method(self, row, column):
         # Find Username field by ID, and populate it with Correct Username
         usernameField = driver.find_element(By.ID, "textfield-1025-inputEl")
-        usernameField.send_keys("pfadmin")
+        usernameField.send_keys("admin")
         # Find Password field by ID, and populate it with Correct Password
         passwordField = driver.find_element(By.ID, "textfield-1026-inputEl")
         passwordField.send_keys("password")
